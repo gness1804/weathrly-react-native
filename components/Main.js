8acc356a7703732dae69616313dbdbce0880d5d5
gness1804 <graham.nessler@gmail.com>
2017-03-20 20:31:54 -0600

    Add Main component

---
+++ components/Main.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import {
+  Text,
+  View
+} from 'react-native';
+
+class Main extends Component {
+
+  render() {
+    return (
+      <View>
+      <Text>Hello! I am the Main component.</Text>
+      </View>
+    );
+  }
+
+}
+
+export default Main;
--- index.android.js
+++ index.android.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Header from './components/Header';
 import {
   AppRegistry,
   StyleSheet,
@@ -7,12 +6,15 @@ import {
   View
 } from 'react-native';
 import styles from "./styles/index-android-styles";
+import Header from './components/Header';
+import Main from './components/Main';
 
 export default class weathrly extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Header />
+        <Main />
       </View>
     );
   }

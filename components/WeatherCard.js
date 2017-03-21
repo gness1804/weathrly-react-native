e1dd39fb1f9f38ac422f283dd6147b95c83b98c8
gness1804 <graham.nessler@gmail.com>
2017-03-20 22:11:22 -0600

    Add forecast

--- components/WeatherCard.js
+++ components/WeatherCard.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import {
   Text,
   View,
+  Image
 } from 'react-native';
 
 const WeatherCard = ({...props}) => {
-  // console.log(props)
   return (
     <View>
     <Text>Time Period: {props.title}</Text>
+    <Text>Forecast: {props.fcttext}</Text>
     </View>
   );
 }

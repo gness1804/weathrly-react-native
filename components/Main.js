e527f699579de2ca6943339581e9fd57f69f251c
gness1804 <graham.nessler@gmail.com>
2017-03-20 20:52:57 -0600

    Add url and ready for API call

--- components/Main.js
+++ components/Main.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
 import {
   Text,
-  View
+  View,
+  Button
 } from 'react-native';
+import axios from 'axios';
 
 class Main extends Component {
+    constructor(){
+      super()
+      this.state = {
+        weather: [],
+        location: 'Denver',
+        state: 'CO',
+      }
+    }
+
+  getWeather = () => {
+    const city = this.state.location.toLowerCase();
+    const state = this.state.state;
+    const url = `http://api.wunderground.com/api/47fe8304fc0c9639/forecast/q/${state}/${city}.json`
+
 
   render() {
     return (
       <View>
-      <Text>Hello! I am the Main component.</Text>
+        <Button
+          title="Get Weather"
+          onPress={() => { this.getWeather() }}
+        >
+        </Button>
       </View>
     );
   }

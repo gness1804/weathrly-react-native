1501ab6647277207b9e74210d03c411d7f10045a
gness1804 <graham.nessler@gmail.com>
2017-03-20 21:05:23 -0600

    Add basic API call

--- components/Main.js
+++ components/Main.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import {
   Text,
   View,
-  Button
+  Button,
+  AsyncStorage,
 } from 'react-native';
 import axios from 'axios';
 
@@ -20,7 +21,13 @@ class Main extends Component {
     const city = this.state.location.toLowerCase();
     const state = this.state.state;
     const url = `http://api.wunderground.com/api/47fe8304fc0c9639/forecast/q/${state}/${city}.json`
-
+    axios.get(url)
+    .then((data) => {
+      this.setState({weather: data.data.forecast.txt_forecast.forecastday})
+    })
+    .then(() => {AsyncStorage.setItem('city', city)})
+    .then(() => {AsyncStorage.setItem('state', state)})
+  }
 
   render() {
     return (

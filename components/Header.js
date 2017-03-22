5ca029e3a77c61bb17c54a62dc348c3dc8f7db2e
gness1804 <graham.nessler@gmail.com>
2017-03-22 17:52:14 -0600

    Add Weather Underground logo and credit

--- components/Header.js
+++ components/Header.js
@@ -2,6 +2,9 @@ import React from 'react';
 import {
   Text,
   View,
+  Image,
+  Linking,
+  TouchableOpacity,
 } from 'react-native';
 import styles from '../styles/header-styles';
 
@@ -10,6 +13,15 @@ const Header = () => {
     <View style={styles.container}>
       <Text style={styles.headline}>Welcome to Weathrly</Text>
       <Text style={styles.tagline}>Your World, Your Weather.</Text>
+      <TouchableOpacity
+        onPress={() => { Linking.openURL('https://www.wunderground.com/') }}
+      >
+        <Image
+          style={styles.pic}
+          source={{ uri: 'https://icons.wxug.com/logos/PNG/wundergroundLogo_4c.png' }}
+        />
+      </TouchableOpacity>
+      <Text style={styles.credit}>Data courtesy of Weather Underground.</Text>
     </View>
   );
 }
--- styles/header-styles.js
+++ styles/header-styles.js
@@ -1,6 +1,10 @@
 const styles = {
   container: {
+    alignItems: 'center',
     marginBottom: 20,
+  },
+  credit: {
+    marginTop: 10,
   },
   headline: {
     fontSize: 30,
@@ -8,6 +12,11 @@ const styles = {
     marginBottom: 20,
     marginTop: 20,
     textAlign: 'center',
+  },
+  pic: {
+    height: 70,
+    marginTop: 10,
+    width: 100,
   },
   tagline: {
     fontSize: 20,
